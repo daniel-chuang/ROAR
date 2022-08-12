9fd1346ab0953e3ad3923814d42cc57cb07ee822
Daniel Chuang <sharpincludecode@gmail.com>
2022-08-11 17:20:59 -0700

    refactored

--- ROAR/agent_module/special_agents/waypoint_generating_agent.py
+++ ROAR/agent_module/special_agents/waypoint_generating_agent.py
@@ -5,7 +5,8 @@ from ROAR.configurations.configuration import Configuration as AgentConfig
 from pathlib import Path
 
 # showing map
-from ROAR.utilities_module.waypoint_tuning import show_map, prep_map
+from ROAR.utilities_module.waypoint_tuning import show_map, prep_map_visualization
+import os
 
 class WaypointGeneratingAgent(Agent):
     def __init__(self, vehicle: Vehicle, agent_settings: AgentConfig, **kwargs):
@@ -14,7 +15,7 @@ class WaypointGeneratingAgent(Agent):
         if self.output_folder_path.exists() is False:
             self.output_folder_path.mkdir(exist_ok=True, parents=True)
         self.output_file = self.output_file_path.open('w')
-        self.map = prep_map()
+        self.map = prep_map_visualization(os.path.join("data", "birds_eye_map.npy"), os.path.join("data", "checkpoints.csv"))
 
 
     def run_step(self, sensors_data: SensorsData,
@@ -25,7 +26,11 @@ class WaypointGeneratingAgent(Agent):
         # Showing minimap
         # Showing live minimap
         car_coords = [float(i) for i in self.vehicle.transform.record().split(",")]
-        self.map = show_map(self.map, (car_coords[0], car_coords[2]), self.vehicle.get_speed(self.vehicle))
+        self.speed = self.vehicle.get_speed(self.vehicle)
+        self.throttle = self.vehicle.control.throttle
+        self.car_coords = [float(i) for i in self.vehicle.transform.record().split(",")][0:3:2]
+        self.map = show_map(self.map, self.car_coords, self.speed, self.throttle)
+        #show_lane(self.lane_map, self.car_coords, self.speed, self.throttle)
         self.transform_history.append(self.vehicle.transform)
 
         if self.time_counter > 1:
