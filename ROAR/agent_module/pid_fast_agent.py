68138092fd02e82f1d0106a61df72346b1f3d06f
Daniel Chuang <sharpincludecode@gmail.com>
2022-08-19 23:54:54 -0700

    added competitive mode switch

--- ROAR/agent_module/pid_fast_agent.py
+++ ROAR/agent_module/pid_fast_agent.py
@@ -13,6 +13,9 @@ import logging
 from ROAR.utilities_module.waypoint_tuning import *
 import os
 
+# SWITCH OFF FOR SUBMISSIONS
+competitive_mode = True
+
 class PIDFastAgent(Agent):
     def __init__(self, target_speed=40, **kwargs):
         super().__init__(**kwargs)
@@ -40,58 +43,66 @@ class PIDFastAgent(Agent):
         self.logger.debug(
             f"Waypoint Following Agent Initiated. Reading f"
             f"rom {self.route_file_path.as_posix()}")
-        
+
         # For waypoint tuning
         self.checkpoints = prep_checkpoints(os.path.join("ROAR", "datasets", "checkpoints.csv"))
-
-        # For map visualizations
-        self.map = prep_map_visualization(os.path.join("ROAR", "datasets", "birds_eye_map.npy"), os.path.join("ROAR", "datasets", "checkpoints.csv"))
-        self.lane_map = prep_map_visualization(os.path.join("ROAR", "datasets", "birds_eye_map.npy"), os.path.join("ROAR", "datasets", "checkpoints.csv"))
-
-        # For returning current time
-        self.return_time = False
-        self.times_list = []
-        self.time_var = -5
         self.most_recent_checkpoint = -1
-        self.start_time = 0
+
+        if not competitive_mode:
+
+            # For map visualizations
+            self.map = prep_map_visualization(os.path.join("ROAR", "datasets", "birds_eye_map.npy"), os.path.join("ROAR", "datasets", "checkpoints.csv"))
+            self.lane_map = prep_map_visualization(os.path.join("ROAR", "datasets", "birds_eye_map.npy"), os.path.join("ROAR", "datasets", "checkpoints.csv"))
+
+            # For returning current time
+            self.return_time = False
+            self.times_list = []
+            self.time_var = -5
+            self.start_time = 0
 
     def call_in_carla_runner_to_return_time(self):
-        return self.return_time
+        if not competitive_mode:
+            return self.return_time
 
     def get_diff_in_times(self):
-        times_list_diffs = []
-        for i in range(len(self.times_list) - 1):
-            times_list_diffs.append(self.times_list[i + 1] - self.times_list[i])
-        return(times_list_diffs)
+        if not competitive_mode:
+            times_list_diffs = []
+            for i in range(len(self.times_list) - 1):
+                times_list_diffs.append(self.times_list[i + 1] - self.times_list[i])
+            return(times_list_diffs)
 
     def run_step(self, vehicle: Vehicle,
                  sensors_data: SensorsData) -> VehicleControl:
         super(PIDFastAgent, self).run_step(vehicle=vehicle,
                                        sensors_data=sensors_data)
         
-        # Showing live minimap
-        self.speed = self.vehicle.get_speed(self.vehicle)
-        self.throttle = self.vehicle.control.throttle
+
         self.car_coords = [float(i) for i in self.vehicle.transform.record().split(",")][0:3:2]
-        self.map = show_map(self.map, self.car_coords, self.speed, self.throttle)
-        #show_lane(self.lane_map, self.car_coords, self.speed, self.throttle)
+
+        if not competitive_mode:
+            # Showing live minimap
+            self.speed = self.vehicle.get_speed(self.vehicle)
+            self.throttle = self.vehicle.control.throttle
+            self.map = show_map(self.map, self.car_coords, self.speed, self.throttle)
+            #show_lane(self.lane_map, self.car_coords, self.speed, self.throttle)
 
         # Checking for checkpoint
         # Maining time list
-        self.return_time = False
         cur_checkpoint = checkpoint(self.checkpoints, self.car_coords)
+        if not competitive_mode:
+            self.return_time = False
 
-        if len(self.times_list) == 0.0:
-            self.start_time = self.time_var
+            if len(self.times_list) == 0.0:
+                self.start_time = self.time_var
 
-        if self.time_var - self.start_time not in self.times_list and cur_checkpoint == self.most_recent_checkpoint:
-            self.times_list.append(self.time_var - self.start_time)
-            self.times_list_diffs = self.get_diff_in_times()
+            if self.time_var - self.start_time not in self.times_list and cur_checkpoint == self.most_recent_checkpoint:
+                self.times_list.append(self.time_var - self.start_time)
+                self.times_list_diffs = self.get_diff_in_times()
 
-            print("Times", self.times_list)
-            print("Time Diffs", self.times_list_diffs)
-            print("Total Time", sum(self.times_list_diffs))
-            print("\n")
+                print("Times", self.times_list)
+                print("Time Diffs", self.times_list_diffs)
+                print("Total Time", sum(self.times_list_diffs))
+                print("\n")
 
         if cur_checkpoint is not None and cur_checkpoint != self.most_recent_checkpoint:
             print(f"AT CHECKPOINT {cur_checkpoint}")
